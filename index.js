bd2828df9f07ae3b9272ff413c798eb02f81548e
bijayabc <bijayabc1234@gmail.com>
2025-02-24 18:52:21 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+// load express, initiate app
+const express = require('express')
+
+const PORT = 3000
+
+app = express();
+
+// static files
+app.use(express.static('public'));
+
+// dynamic content routes
+var hits = {};
+app.get('/hits/:pageId', (req, res) => {
+    const pageHits = (hits[req.params.pageId] || 0) + 1
+    hits[req.params.pageId] = pageHits
+    res.send((pageHits).toString());
+});
+
+// default route
+app.all('*',(req,res)=>{res.status(404).send('Invalid URL.')});
+
+// start server
+app.listen(PORT, console.log("Server started http://localhost:" + PORT));
